a010d3d445e747aa2db4f68234cbaca1ba9a8ab1
xiao huama <wwwhowardwww@hotmail.com>
2015-08-27 10:17:12 +0800

    init

---
+++ Source/iOS/MXHFrameWork/MXHFrameWork/MXHFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  MXHFrameWork.h
+//  MXHFrameWork
+//
+//  Created by xiao huama on 15/8/27.
+//  Copyright (c) 2015年 xiao huama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MXHFrameWork.
+FOUNDATION_EXPORT double MXHFrameWorkVersionNumber;
+
+//! Project version string for MXHFrameWork.
+FOUNDATION_EXPORT const unsigned char MXHFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MXHFrameWork/PublicHeader.h>
+
+
